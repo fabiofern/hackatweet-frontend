175017b9c1113eb2f4d0d0785a9c86464429f65a
jm.yakan@gmail.com <jm.yakan@gmail.com>
2025-02-13 18:44:13 +0100

    hackatweet frontend page d'acceuil

---
+++ components/Tweet.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Tweet.module.css';
+
+function Tweets() {
+    return (
+      <div>
+        <h1>Bienvenue sur la page des Tweets !</h1>
+        <p>Ici, tu verras tous les tweets des utilisateurs.</p>
+      </div>
+    );
+  }
+  
+  export default Tweets;
---
+++ pages/allTweets.js
@@ -0,0 +1,7 @@
+import Tweet from '../components/Tweet';
+
+function allTweets() {
+  return <Tweet/>;
+}
+
+export default allTweets;
